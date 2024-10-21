acd43045dc2c34190e5c4d55b90035418f2810bc
Tavin Sowell <tavinsowell@gmail.com>
2024-10-21 04:44:19 -0600

    e2e test finished

--- Develop/cypress/e2e/integration/api.cy.ts
+++ Develop/cypress/e2e/integration/api.cy.ts
@@ -1,67 +1,24 @@
 describe('API Requests', () => {
   
-  interface Answer {
-  text: string;
-  isCorrect: boolean;
-  }
-  
-  interface Question {
-  question: string;
-  answers: Answer[];
-  }
-
-  const questions: Question[] = [
-  {
-    question: "What is the output of print(2 ** 3)?",
-    answers: [
-    { text: "6", isCorrect: false },
-    { text: "8", isCorrect: true },
-    { text: "9", isCorrect: false },
-    { text: "12", isCorrect: false }
-    ]
-  },
-  {
-    question: "Which of the following is a mutable data type in Python?",
-    answers: [
-    { text: "str", isCorrect: false },
-    { text: "tuple", isCorrect: false },
-    { text: "list", isCorrect: true },
-    { text: "int", isCorrect: false }
-    ]
-  },
-  {
-    question: "What is the keyword used to define a function in Python?",
-    answers: [
-    { text: "function", isCorrect: false },
-    { text: "func", isCorrect: false },
-    { text: "def", isCorrect: true },
-    { text: "define", isCorrect: false }
-    ]
-  },
-  {
-    question: "Which of the following is used to create an empty set?",
-    answers: [
-    { text: "{}", isCorrect: false },
-    { text: "[]", isCorrect: false },
-    { text: "set()", isCorrect: true },
-    { text: "()", isCorrect: false }
-    ]
-  },
-  ];
-
-  context("random questions", () => {
   beforeEach(() => {
-    cy.intercept("GET", "/api/questions/random", {
-    body: questions,
+    cy.visit("/");
+    cy.intercept({
+      method:"GET", 
+      url: "/api/questions/random"
+    }, {
+    fixture: "questions.json",
+    statusCode: 200
+    }).as("getQuestions");
     });
   });
 
-  it('GET /api/questions/random', () => {
+  it('should get questions properly', () => {
     cy.request('/api/questions/random')
-    .its('body')
-    .should('be.an', 'array')
-    .and('have.length', questions.length)
-    .and('deep.include.members', questions);
-  });
-  });
-});
+      .then((response) => {
+        expect(response.status).to.eq(200);
+        expect(response.body[0]).to.have.property('question');
+        expect(response.body[0]).to.have.property('answers');
+      
+            });
+        });
+
